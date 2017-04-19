21d09ba4ec43c4afb90ddaad2667ffbb5f492c51
Hayley Kwan <hayleykwan419@gmail.com>
2017-04-19 23:56:05 +0100

    pug template engine all figured out

--- app.js
+++ app.js
@@ -13,8 +13,8 @@ var board = require('./routes/board');
 var app = express();
 
 // view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,8 @@ console.log('Get index page ready');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   console.log('GET request, going to render index (NOT pug) file');
-  // res.render('index', { title: 'Ideator' });
-  res.render('index');
+  res.render('index', { title: 'Ideator' });
+  // res.render('index');
 });
 
 module.exports = router;
